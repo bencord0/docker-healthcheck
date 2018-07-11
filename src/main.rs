a1474ee985d89ffdc60382fcf559fbbe85b091a4
Ben Cordero <bencord0@condi.me>
2018-07-11 23:13:45 +0100

    Use a service_fn instead of service_fn_ok

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+extern crate futures;
 extern crate hyper;
 extern crate http;
 extern crate rust_docker;
 
+use futures::future;
 use http::{Request, Response, StatusCode};
-use hyper::{Body, Server};
+use hyper::{Body, Method, Server};
 use hyper::rt::Future;
-use hyper::service::service_fn_ok;
+use hyper::service::service_fn;
 use rust_docker::api::version::Version;
 use rust_docker::DockerClient;
 
@@ -27,27 +29,33 @@ fn is_docker_ok() -> bool {
     };
 }
 
-fn health(_req: Request<Body>) -> Response<Body> {
-    let mut response = Response::default();
 
+fn health(_request: &Request<Body>, response: &mut Response<Body>) {
     if is_docker_ok() {
         *response.status_mut() = StatusCode::OK;
     } else {
         *response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
     }
+}
 
-    return response;
+type FutureResponse = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
+fn router(request: Request<Body>) -> FutureResponse {
+    let mut response = Response::default();
+
+    match (request.method(), request.uri().path()) {
+        (&Method::GET, "/") => {},
+        (&Method::GET, "/health") => health(&request, &mut response),
+        _ => *response.status_mut() = StatusCode::NOT_FOUND,
+    }
+
+    return Box::new(future::ok(response));
 }
 
 fn main() {
     let addr = ([0, 0, 0, 0], 8000).into();
 
-    let svc = || {
-        service_fn_ok(health)
-    };
-
     let server = Server::bind(&addr)
-        .serve(svc)
+        .serve(|| service_fn(router))
         .map_err(|e| eprintln!("server error: {}", e));
 
     println!("Starting server on: {}", addr);
